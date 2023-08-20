c1cef8a8ff1367e39f6d423eaea3f7df000dd9c6
LdBeth <andpuke@foxmail.com>
2023-08-20 18:43:58 -0500

    try hlt

---
+++ queries/highlights.scm
@@ -0,0 +1,27 @@
+;; keywords
+[
+"attribute"
+"default"
+"datatypes"
+"div"
+"element"
+"empty"
+"external"
+"grammar"
+"include"
+"inherit"
+"list"
+"mixed"
+"namespace"
+"notAllowed"
+"parent"
+"start"
+"string"
+"text"
+"token" ] @keyword
+
+(datatype_name) @type
+
+(literal_segment) @number
+
+(define name: (identifier) @function)
